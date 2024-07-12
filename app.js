b20f0c351ddcf9ea4749d8b3066086108308b0cb
Franklin Kong <franklinkong981@gmail.com>
2024-07-12 16:12:12 -0700

    Finished project

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const getCards = document.querySelector(".get-cards");
+const pokemonCards = document.querySelector(".pokemon-cards");
 let totalPokemon;
 
 getCards.addEventListener('click', () => {
@@ -6,27 +7,39 @@ getCards.addEventListener('click', () => {
     //generate the names and links for all available Pokemon in the API.
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=2000&offset=0')
         .then(res => {
-            pokemonResults = res.data.results;
+            let pokemonResults = res.data.results;
             totalPokemon = pokemonResults.length;
             //select 3 random Pokemon
             for (let i = 1; i < 4; i++) {
-                pokemonSelected = Math.floor(Math.random() * totalPokemon);
+                let pokemonSelected = Math.floor(Math.random() * totalPokemon);
                 threeRandomPokemon.push(axios.get(pokemonResults[pokemonSelected].url));
             }
             return Promise.all(threeRandomPokemon);
         })
         .then(threeRandomPokemon => {
-            for (let i = 0; i < 4; i++) {
-                currentPokemon = threeRandomPokemon[i].data;
+            for (let i = 0; i < 3; i++) {
+                let currentPokemonCard = pokemonCards.children[i];
+                let currentPokemonCardImageElement = currentPokemonCard.children[0];
+                let currentPokemonCardTitleElement = currentPokemonCard.children[1].children[0];
+                let currentPokemonCardDescriptionElement = currentPokemonCard.children[1].children[1];
+
+                let currentPokemon = threeRandomPokemon[i].data;
+                let currentPokemonImageURL = currentPokemon.sprites.front_default;
+                currentPokemonCardImageElement.setAttribute('src', currentPokemonImageURL);
                 //send request to species route of selected Pokemon to obtain Pokemon description.
                 axios.get(currentPokemon.species.url)
                     .then(res => {
-                        console.log("You selected the Pokemon ", res.data.name);
-                        textEntries = res.data.flavor_text_entries;
+                        let currentPokemonName = res.data.name;
+                        console.log("You selected the Pokemon ", currentPokemonName);
+                        currentPokemonCardImageElement.setAttribute('alt', currentPokemonName);
+                        currentPokemonCardTitleElement.innerText = currentPokemonName;
+                        
+                        let textEntries = res.data.flavor_text_entries;
                         //traverse the text entries until you find an English description.
                         for (let i = 0; i < textEntries.length; i++) {
                             if (textEntries[i].language.name == "en") {
-                                console.log(textEntries[i].flavor_text);
+                                let currentPokemonDescription = textEntries[i].flavor_text;
+                                currentPokemonCardDescriptionElement.innerText = currentPokemonDescription;
                                 break;
                             }
                         }
